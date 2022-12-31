fe6eca186d627002734ca1b2b74b2f4ce3da1582
Vasyl <v.antoniukp@gmail.com>
2022-12-31 12:29:46 +0000

    First commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+app.get("/", function(req, res){
+    res.sendFile(__dirname + '/index.html');
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT || 3000, function () {      
+    console.log(`Server is runnig on port: 3000`);
+  });
